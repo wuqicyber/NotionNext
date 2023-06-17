66a16a1483014b92d62f07d35e276e42e270b6b4
wuqicyber <62527188+wuqicyber@users.noreply.github.com>
2023-06-17 16:10:53 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -19,7 +19,7 @@ const BLOG = {
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '很久不是自己', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '我能做的不过是静候花瓣慢慢脱落', // 作者简介
-  LINK: process.env.NEXT_PUBLIC_LINK || 'https://lac.rottenix.top', // 网站地址
+  LINK: process.env.NEXT_PUBLIC_LINK || 'https://weibo.com', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Notion, 博客', // 网站关键词 英文逗号隔开
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
   CONTACT_EMAIL: process.env.NEXT_PUBLIC_CONTACT_EMAIL || '', // 邮箱地址 例如mail@tangly1024.com
